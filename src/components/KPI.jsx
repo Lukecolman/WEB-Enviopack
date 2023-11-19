d175d88921cc8ebeb774092fc68f648fff26126f
Lukecolman <79427199+Lukecolman@users.noreply.github.com>
2023-11-18 21:30:56 -0300

    KPI styles to scss

--- src/components/KPI.jsx
+++ src/components/KPI.jsx
@@ -9,12 +9,12 @@ const KPI = () => {
         <>
             <div>KPI</div>
             <section className='flexCenter'>
-                <div className='w-[1512px] z-10 pl-[188.17px] pr-[191.83px] pt-[76.65px] pb-[160.19px] bg-gradient-to-br flex-col justify-start items-center gap-[64.37px] inline-flex bg-gray-600'>
-                    <div className='h-[158.79px] pb-[4.79px] flex-col justify-start items-center gap-4 inline-flex'>
-                        <div className="text-center text-slate-700 text-5xl font-semibold font-['Outfit'] leading-[78px]">
+                <div className='kpi-container'>
+                    <div className='kpi-text-box'>
+                        <div className="kpi-title">
                             {t('kpi.title')}
                         </div>
-                        <div className="w-[723px] text-center text-slate-700 text-lg font-normal font-['DM Sans'] leading-[30px]">
+                        <div className="kpi-content">
                             {t('kpi.content')}
                         </div>
                     </div>
